704b0306e0488e66ef325f11818348acb295b3a9
Nikita Mihalyov <nikita.mihalyov@gmail.com>
2020-04-15 21:25:51 +0300

    Manufacturers

--- dev/components/banner/banner.js
+++ dev/components/banner/banner.js
@@ -5,7 +5,5 @@ $('.js-banner-slider').slick({
   prevArrow: '<button class="banner__arrow banner__arrow--prev"></button>',
   nextArrow: '<button class="banner__arrow banner__arrow--next"></button>',
   dotsClass: 'banner__dots',
-  customPaging: () => {
-    return '';
-  }
+  customPaging: () => ''
 });
