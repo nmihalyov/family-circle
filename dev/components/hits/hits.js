e5a7ad4aeb9abcf5a5dcf2d8fc84b4188845ea0e
Nikita Mihalyov <nikita.mihalyov@gmail.com>
2020-04-20 11:23:33 +0300

    Scheme & Feedback-calc & Description

--- dev/components/hits/hits.js
+++ dev/components/hits/hits.js
@@ -2,6 +2,7 @@
 $('.js-hits-slider').slick({
   infinite: true,
   slidesToShow: 4,
+  swipeToSlide: true,
   prevArrow: '<button class="hits__arrow hits__arrow--prev"></button>',
   nextArrow: '<button class="hits__arrow hits__arrow--next"></button>',
   appendArrows: '.hits__slider'
